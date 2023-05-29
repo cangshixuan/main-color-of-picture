38ea69f8daf05626f880dd87740883dbb6c5bee0
Sylar <Sylar@fanruan.com>
2023-05-29 10:03:05 +0800

    feat: 获取图片主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import ColorThief from "../utils/ColorThief.js";
+
 export function App() {
+  const colorThief = new ColorThief();
+
+  const handleMouseEnter = (e: MouseEvent) => {
+    const colors = colorThief.getPalette(e.target, 3, 10);
+    const rgbList = colorThief.convertColorRgb(colors);
+    const gradient = `linear-gradient(to bottom, ${rgbList[0]}, ${rgbList[1]}, ${rgbList[2]})`;
+    document.body.style.background = gradient;
+  };
+
+  const handleMouseLeave = () => {
+    document.body.style.background = "#fff";
+  };
+
   return (
     <div className="h-full w-full flex items-center justify-center ">
       <div className="grid gap-20 grid-cols-3 cursor-pointer">
@@ -8,9 +23,12 @@ export function App() {
               style={{
                 height: "400px",
               }}
+              crossOrigin="anonymous"
               key={index}
               alt="图片"
               src={`https://picsum.photos/800/800`}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             ></img>
           );
         })}
